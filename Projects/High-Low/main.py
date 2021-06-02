1e32ee910b96cc70ec00e8b0d49eefe08ed7731d
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2021-06-02 18:54:56 +1200

    Added max guesses and best guess

--- Projects/High-Low/main.py
+++ Projects/High-Low/main.py
@@ -22,6 +22,7 @@ def ask_for_int(question: str) -> int:
 def show_instructions() -> None:
 	print("----- How To Play -----")
 	print("A number between 1 and 100 will be chosen")
+	print("You have 7 guesses")
 	print("You have to gess a number and the program will say higher or lower")
 	print("Then you know if you need to go higher or lower for your guess")
 	print("If you guess the number you win!")
@@ -34,14 +35,31 @@ def main() -> None:
 	secret: int = random.randint(1, 100)
 
 	guess: int = -1
-	while guess != secret:
+	best_guess: int = -1
+	best_guess_difference: int = -1
+
+	guesses: int = 0
+	while True:
 		guess = ask_for_int("Enter your guess: ")
 
 		if guess < secret:
 			print("Higher")
 		elif guess > secret:
 			print("Lower")
-	print("You found the secret number!")
+		else:
+			print("You found the secret number!")
+			return
+
+		if abs(guess - secret) < best_guess_difference or best_guess == -1:
+			best_guess_difference = abs(guess - secret)
+			best_guess = guess
+
+		guesses += 1
+		if guesses >= 7:
+			print("You are out of guesses")
+			print(f"The secret was {secret}")
+			print(f"Your best guess was {best_guess}")
 
 if __name__ == "__main__":
-	main()
+	while True:
+		main()
